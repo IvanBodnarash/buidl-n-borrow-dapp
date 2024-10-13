88c1248f545b0ee8b9b8f97cef907da0841b4f31
unknown <bodnarash1999@gmail.com>
2024-10-13 22:25:02 +0200

    mobile styles adjustments

--- client-copy/tailwind.config.js
+++ client-copy/tailwind.config.js
@@ -2,7 +2,43 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: 0,
+          },
+          "100%": {
+            opacity: 1,
+          },
+        },
+        "fade-in-left": {
+          "0%": {
+            opacity: 0,
+            transform: "translate3d(-100%, 0, 0)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
+        "fade-in-right": {
+          "0%": {
+            opacity: 0,
+            transform: "translate3d(100%, 0, 0)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
+      },
+      animation: {
+        fadein: 'fade-in 1s ease-in-out 0.25s 1',
+        fadeinleft: 'fade-in-left 1s ease-in-out 0.25s 1',
+        fadeinright: 'fade-in-right 1s ease-in-out 0.25s 1',
+      },
+    },
   },
   plugins: [],
-}
+};
